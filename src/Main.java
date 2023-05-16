3769d0ea36d187150a63c4dad9beca533a4f6bfc
jillmdev <jill.ochieng@strathmore.edu>
2023-05-16 13:06:36 +0300

    Arrays in Java
    Prints all elements of an array

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+
+public class Main{
+    public static void main(String[] args){
+        int []age={1,2,3,4,4,5,};
+        for(int i=0;i<age.length;i++);
+        System.out.println(age[i]);
+    }
+
+}
